90de1ade1aa05dda48182e629bf23b950b95b5b1
Thibault Seisel <seisel.thibault@gmail.com>
2020-02-02 19:30:12 +0100

    Initialized template project from Spring Generator

---
+++ src/main/kotlin/com/github/thibseisel/music/OdeonApplication.kt
@@ -0,0 +1,11 @@
+package com.github.thibseisel.music
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OdeonApplication
+
+fun main(args: Array<String>) {
+	runApplication<OdeonApplication>(*args)
+}
