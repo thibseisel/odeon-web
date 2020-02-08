d554d7431998d60242679dce0e645442a20373ce
Thibault Seisel <seisel.thibault@gmail.com>
2020-02-08 11:19:53 +0100

    Rewrote mapToDefined operator to follow RxJs guide

--- web/src/app/rx-operators.ts
+++ web/src/app/rx-operators.ts
@@ -1,4 +1,4 @@
-import { OperatorFunction, Observable } from 'rxjs'
+import { OperatorFunction, Observable } from 'rxjs';
 
 /**
  * Transform items from that stream by applying the provided transform, 
@@ -7,26 +7,27 @@ import { OperatorFunction, Observable } from 'rxjs'
  * @param transform The transform function to apply to each element.
  */
 export function mapIfDefined<T, R extends Object>(
-    transform: (element: T) => R | null | undefined
-  ): OperatorFunction<T, R> {
-    return (source: Observable<T>) => {
-      return new Observable<R>(observer => {
-        return source.subscribe({
-          next(element: T) {
-            const transformedElement = transform(element)
+  transform: (element: T) => R | null | undefined
+): OperatorFunction<T, R> {
+  return (source: Observable<T>) => {
+    return new Observable<R>(subscriber => {
+
+      const subscription = source.subscribe({
+        next: (element: T) => {
+          try {
+            const transformedElement = transform(element);
             if (transformedElement) {
-              observer.next(transformedElement)
+              subscriber.next(transformedElement);
             }
-          },
-    
-          error(error) {
-            observer.error(error)
-          },
-    
-          complete() {
-            observer.complete()
+          } catch (userError) {
+            subscriber.error(userError);
           }
-        })
-      })
-    }
-  }
+        },
+        error: (upstreamError) => subscriber.error(upstreamError),
+        complete: () => subscriber.complete()
+      });
+
+      return subscription;
+    });
+  };
+}
