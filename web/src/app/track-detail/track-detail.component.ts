fd242b314edba61171c32c6b10e53c2fd85b8d38
Thibault Seisel <seisel.thibault@gmail.com>
2020-02-16 16:42:03 +0100

    Display of the tone in track detail

--- web/src/app/track-detail/track-detail.component.ts
+++ web/src/app/track-detail/track-detail.component.ts
@@ -8,4 +8,50 @@ import { Track } from "../track-models";
 })
 export class TrackDetailComponent {
   @Input() track?: Track;
+
+  public get tone(): string | null {
+    if (this.track) {
+      const key = this.track.features.key;
+      const mode = this.track.features.mode;
+      return `${codeToKeyName(key)}${codeToMode(mode)}`;
+
+    } else {
+      return null;
+    }
+  }
+}
+
+function codeToMode(mode: number): string {
+  return (mode > 0) ? "M" : "m";
+}
+
+function codeToKeyName(key: number | undefined): string {
+  switch (key) {
+    case 0:
+      return "C";
+    case 1:
+      return "C#";
+    case 2:
+      return "D";
+    case 3:
+      return "D#";
+    case 4:
+      return "E";
+    case 5:
+      return "F";
+    case 6:
+      return "F#";
+    case 7:
+      return "G";
+    case 8:
+      return "G#";
+    case 9:
+      return "A";
+    case 10:
+      return "A#";
+    case 11:
+      return "B";
+    default:
+      return "?";
+  }
 }
