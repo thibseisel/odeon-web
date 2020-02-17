f7efdf3b47e3e68a6c05efb127b76c74ba6ad6dc
Thibault SEISEL <seisel.thibault@gmail.com>
2020-02-17 16:52:14 +0100

    Simplify HttpStatusSerializer
    There is no need to handle the case where the source HttpStatus is null.
    Jackson documentation states that the value is never null.

--- src/main/kotlin/com/github/thibseisel/music/JsonSerializers.kt
+++ src/main/kotlin/com/github/thibseisel/music/JsonSerializers.kt
@@ -5,13 +5,12 @@ import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.SerializerProvider
 import org.springframework.http.HttpStatus
 
+/**
+ * A custom JSON serializer that maps constants from [HttpStatus] to their equivalent HTTP status code.
+ */
 class HttpStatusSerializer : JsonSerializer<HttpStatus>() {
 
-    override fun serialize(value: HttpStatus?, gen: JsonGenerator, serializers: SerializerProvider) {
-        if (value != null) {
-            gen.writeNumber(value.value())
-        } else {
-            gen.writeNull()
-        }
+    override fun serialize(value: HttpStatus, gen: JsonGenerator, serializers: SerializerProvider) {
+        gen.writeNumber(value.value())
     }
 }
