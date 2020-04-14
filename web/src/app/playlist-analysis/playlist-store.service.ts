043e99843757597ea49774bd63b3963450142433
Thibault Seisel <seisel.thibault@gmail.com>
2020-04-14 08:57:44 +0200

    Fix missing album artwork for playlist tracks

--- web/src/app/playlist-analysis/playlist-store.service.ts
+++ web/src/app/playlist-analysis/playlist-store.service.ts
@@ -5,7 +5,7 @@ import { Observable, of, zip } from "rxjs";
 import { RemotePlaylist } from "./remote-playlist";
 import { catchError, concatMap, map } from "rxjs/operators";
 import { Track } from "../track-analysis/track-models";
-import { RemoteTrack, AudioFeature } from "../track-analysis/remote-models";
+import { RemoteTrack, AudioFeature, ImageSpec } from "../track-analysis/remote-models";
 
 @Injectable({
   providedIn: 'root'
@@ -66,14 +66,33 @@ export class PlaylistStoreService {
 
   private combineToTracks(tracks: RemoteTrack[], features: AudioFeature[]): Track[] {
     return tracks.map((it, index) => ({
-      id: it.id,
-      name: it.name,
-      artist: it.artists[0]?.name,
-      album: it.album.name,
-      trackNo: it.track_number,
-      duration: it.duration,
-      popularity: it.popularity,
-      features: features[index]
-    }))
+        id: it.id,
+        name: it.name,
+        artist: it.artists[0]?.name,
+        album: it.album.name,
+        trackNo: it.track_number,
+        duration: it.duration,
+        popularity: it.popularity,
+        features: features[index],
+        artworkUrl: findSmallestIcon(it.album.images)?.url
+      })
+    )
+  }
+}
+
+function findSmallestIcon(icons: readonly ImageSpec[]): ImageSpec | null {
+
+  function area(image: ImageSpec): number {
+    if (image.width && image.height) {
+      return image.width * image.height
+    } else {
+      return Number.MAX_VALUE
+    }
+  }
+
+  if (icons.length > 0) {
+    return icons.reduce((smallest, image) => (area(image) < area(smallest)) ? image : smallest)
+  } else {
+    return null
   }
 }
