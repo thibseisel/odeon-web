b349994fffd5db034a498b81bde6fcfc283a0ab4
Thibault SEISEL <seisel.thibault@gmail.com>
2020-04-20 12:24:21 +0200

    Display tempo from 0 to 240 BPM with a step of 15

--- web/src/app/playlist-analysis/playlist-store.service.ts
+++ web/src/app/playlist-analysis/playlist-store.service.ts
@@ -96,6 +96,10 @@ function findSmallestIcon(icons: ReadonlyArray<ImageSpec>): ImageSpec | undefine
   }
 }
 
+const MIN_TEMPO = 0
+const MAX_TEMPO = 240
+const TEMPO_STEP = 15
+
 function combineToPlaylist(playlist: RemotePlaylist, tracks: Array<RemoteTrack>, features: Array<AudioFeature>): Playlist {
   return {
     id: playlist.id,
@@ -113,7 +117,7 @@ function combineToPlaylist(playlist: RemotePlaylist, tracks: Array<RemoteTrack>,
     stats: {
       keys: countKeyOccurences(features),
       modes: countModeOccurences(features),
-      tempo: groupByTempoRange(features, it => it.tempo),
+      tempo: groupByRange(features, it => it.tempo, MIN_TEMPO, MAX_TEMPO, TEMPO_STEP),
       energy: groupByPercentRange(features, it => it.energy),
       danceability: groupByPercentRange(features, it => it.danceability),
       valence: groupByPercentRange(features, it => it.valence)
@@ -145,72 +149,85 @@ function countModeOccurences(features: Array<AudioFeature>): Map<MusicalMode, nu
   return occurencesPerMode
 }
 
-function groupByTempoRange(
-  source: Array<AudioFeature>,
-  selector: (item: AudioFeature) => number
-): ReadonlyArray<DistributionRange> {
-
-  const categories = Array<Mutable<DistributionRange>>()
-  const step = 10
-
-  for (const el of source) {
-    const feature = selector(el)
-    const categoryIndex = Math.floor(feature / step)
-
-    const category = categories[categoryIndex]
-    if (category) {
-      category.count++
-    } else {
-      categories[categoryIndex] = {
-        start: categoryIndex * step,
-        endExclusive: (categoryIndex + 1) * step,
-        count: 1
-      }
-    }
-  }
-
-  for (let index = 0; index < categories.length; index++) {
-    if (!categories[index]) {
-      categories[index] = {
-        start: index * step,
-        endExclusive: (index + 1) * step,
-        count: 0
-      }
-    }
-  }
-
-  return categories
+/**
+ * Make sure that the input number `value` always fits in the range `min` - `max`.
+ */
+function clamp(value: number, minValue: number, maxValue: number): number {
+  return (value < minValue) ? minValue : (value > maxValue) ? maxValue : value
 }
 
-const MIN_PERCENT = 0.0
-const MAX_PERCENT = 1.0
-
-const PERCENT_CATEGORY_COUNT = 10
-
-function groupByPercentRange(
-  source: ReadonlyArray<AudioFeature>,
-  selector: (feature: AudioFeature) => number
+/**
+ * Compute the distribution of values of a specific audio feature from the source set of `features`.
+ *
+ * For example, a call to
+ * ```
+ * groupByRange(features, (feature) => feature.tempo, 60, 180, 15)
+ * ```
+ * will produce an array that looks like the following (`count` values depend on the source set of features):
+ * ```
+ * [
+ *  { start: 60, endExclusive: 75, count: 0 },
+ *  { start: 75, endExclusive: 90, count: 14 },
+ *  { start: 90, endExclusive: 105, count: 29 },
+ *  { start: 105, endExclusive: 120, count: 42 },
+ *  { start: 120, endExclusive: 135, count: 37 },
+ *  { start: 135, endExclusive: 150, count: 21 },
+ *  { start: 150, endExclusive: 165, count: 13 },
+ *  { start: 165, endExclusive: 180, count: 4 },
+ * ]
+ * ```
+ *
+ * @param features The source set of track audio features.
+ * @param selector A function that selects the value of the specific audio feature whose repartition should be computed.
+ * @param min The minimum considered value of the resulting distribution.
+ * Any feature whose value is less than `min` will fall in the lowest category.
+ * @param max The maximum considered value of the resulting distribution.
+ * Any value whose value is more than `max` will fall in the highest category.
+ * @param step The width of each range of the distribution.
+ */
+function groupByRange(
+  features: ReadonlyArray<AudioFeature>,
+  selector: (feature: AudioFeature) => number,
+  min: number,
+  max: number,
+  step: number
 ): ReadonlyArray<DistributionRange> {
 
-  const ranges = Array<Mutable<DistributionRange>>()
-  const step = 1.0 / PERCENT_CATEGORY_COUNT
+  // Due to the inherent imprecise nature of floating point decimals,
+  // we can't use a for loop starting at min and increasing by step until max.
+  // This may lead to an inexact number of columns due to accumulated approximation errors.
+  const rangeWidth = max - min
+  const rangeCount = Math.floor(rangeWidth / step)
 
-  for (let lowerBound = MIN_PERCENT; lowerBound < (MAX_PERCENT - step); lowerBound += step) {
-    ranges.push({
+  const ranges = new Array<Mutable<DistributionRange>>(rangeCount)
+
+  for (let index = 0; index < rangeCount; index++) {
+    const lowerBound = min + index * step
+    ranges[index] = {
       start: lowerBound,
       endExclusive: lowerBound + step,
       count: 0
-    })
+    }
   }
 
-  for (const feature of source) {
-    const value = selector(feature)
-    const clampedValue = (value < MIN_PERCENT) ? MIN_PERCENT : (value > MAX_PERCENT) ? MAX_PERCENT : value
-    const rangeIndex = Math.floor(clampedValue / step)
+  for (const feature of features) {
+    const value = clamp(selector(feature), min, max)
+    const rangeIndex = Math.floor((value - min) / step)
 
     const matchingRange = ranges[rangeIndex]
     matchingRange.count++
   }
 
   return ranges
+}
+
+const MIN_PERCENT = 0.0
+const MAX_PERCENT = 1.0
+const PERCENT_STEP = 0.1
+
+function groupByPercentRange(
+  features: ReadonlyArray<AudioFeature>,
+  selector: (feature: AudioFeature) => number
+): ReadonlyArray<DistributionRange> {
+  return groupByRange(features, selector, MIN_PERCENT, MAX_PERCENT, PERCENT_STEP)
 }
